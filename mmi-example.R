5d9bd01ef880978052a912b1a1845b55a5399b2b
luigi cerulo <luigi.cerulo@gmail.com>
2016-08-14 23:47:01 +0200

    update mmi.R

---
+++ mmi-example.R
@@ -0,0 +1,72 @@
+source("mmi.R")
+
+# Dataset lavoro Gambardella et al. BMC Bioinformatics (2015) 16:279
+#require(R.matlab)
+#d=readMat("ATLAS_dataset.mat",verbose = T)
+#mexp=d$expression
+#allgenes=unlist(d$map.platform)
+#modulators=unlist(d$map.K)
+#tf=c("CDX2","E2F1","ELK1","ETS1","GATA1","GATA2","MYC","SMAD3","SMAD4","STAT1","STAT3","STAT6","TCF4","TP53")
+#rownames(mexp) = allgenes
+#save(tf,modulators,allgenes,mexp,file="ATLAS_dataset.Rdata")
+load(file="ATLAS_dataset.Rdata",verbose=T)
+
+# Recupero goldstandard dalle tabelle del paper
+t=read.table("pathways-genes.txt",sep="\t",header=F,stringsAsFactors = F)
+pwg=strsplit(t$V2,", ")
+names(pwg) = t$V1
+t=read.table("tf-pathways.txt",sep="\t",header=F,stringsAsFactors = F)
+tfpw=lapply(strsplit(t$V2,", "),function(x) gsub(" \\(.*\\)","",x))
+names(tfpw) = t$V1
+tfmodulators = tfpw
+tfmodulators = lapply(tfmodulators, function(x) unique(unlist(pwg[x])))
+
+# rimozione probe con poca variabilita'
+iqr <- apply(mexp, 1, IQR, na.rm = TRUE)
+var.cutoff = quantile(iqr, 0.1, na.rm=TRUE) # percentuale che ne butta fuori
+selected = iqr > var.cutoff
+M <- mexp[selected, ]
+dim(M)
+colnames(M) = paste0("S",1:ncol(M))
+
+# verifica che i geni del goldstandar siano presenti nel dataset
+unique(unlist(tfmodulators)) %in% rownames(M)
+unique(unlist(tfmodulators)) %in% modulators
+names(tfmodulators) %in% rownames(M)
+
+modulators = modulators[modulators %in% rownames(M)]
+tf = tf[tf %in% rownames(M)]
+
+target = setdiff(rownames(M),c(tf,modulators))
+
+# prova con pochi
+target= sample(target,1000)
+#modulators=sample(modulators,3)
+kordering = t(apply(M[modulators,],1,order,decreasing=T))
+colnames(kordering) = colnames(M)
+out = mmi(M,tf = tf,target = target,kordering = kordering,nboot=100,positiveOnly=F,S=10,sig = 0.01,ncore=50)
+
+
+# test prediction performance
+
+# costruzione oracolo lista inversa TF -> modulator
+tfpred =list()
+for(i in names(out)) {
+  for (j in names(out[[i]])) {
+    if (nrow(out[[i]][[j]])>0) {
+      # considero come score del modulatore il minimo del pvalue tra i target regolati dal tf
+      pv=min(out[[i]][[j]][,"FDR"])
+      names(pv) = i
+      tfpred[[j]] = c(tfpred[[j]],pv)
+    }
+  }
+}
+
+require(ROCR)
+for(i in names(tfpred)) {
+  pred = prediction(1-tfpred[[i]],names(tfpred[[i]]) %in% tfmodulators[[i]])
+  perf = performance(pred,measure="prec",x.measure="rec")
+  plot(perf)
+}
+
+
---
+++ mmi.R
@@ -0,0 +1,148 @@
+
+
+
+mmi = function(mexp,tf,target,kordering,alltarget=TRUE,positiveOnly=F,ignore = 0.15,ncore=4,S=5,nboot=100,bfrac=0.8,sig=0.05) {
+  # mexp - matrice di espressione (geni sulle righe, samples sulle colonne)
+  # tf - lista dei TF (deve essere un sottoinsieme di rownames(mexp))
+  # target - lista dei target (deve essere un sottoinsieme di rownames(mexp)\tf)
+  # kordering - matrice degli ordinamenti dei samples dei geni ritenuti modulatori
+  #             (rownames(kordering) deve essere un sottinsieme di rownames(mexp))
+  #             (colnames(kordering) deve essere identico a colnames(mexp)))
+  #             Es.
+  #             kordering fatto sulla metilazione dei target (mmet matrice di metilazione con matched samples)
+  #             kordering = t(apply(mmet[target,],1,order,decreasing=F))
+  #             in questo caso ha senso solo alltarget=FALSE e positiveOnly=T
+  #
+  #             kordering fatto sulla espressione di cofattori che modulano la regolazione TF->target
+  #             kordering = t(apply(mexp[cofactors,],1,order,decreasing=F))
+  #             in questo caso ha senso alltarget=TRUE e positiveOnly=F
+  # alltarget - se uguale a TRUE (default) la mutua inf è calcolata su tutti i target, se FALSE è calcolata solo sul modulatore corrente 
+  #             (utile nel caso di kordering fatto sulla metilazione dei target)
+  # positiveOnly - se uguale a TRUE (default) effettua solo il test (1..k) vs all, altrimenti considera anche l'inverso (k..n) vs all
+  # ignore - percentuale dei sample esterni da ignorare quando si calcola la MI
+  # ncore - numero di core per sfruttare il calcolo parallelo
+  # S - numero di samples da raggruppare per la ricerca di k (T=1 il test è fatto per ogni sample)
+  # nboot - numero di bootstrap per generare le distribuzioni
+  # bfrac - frazione dei sample per il bootstrap
+  # sig - livello di significativita per filtrare i risultati
+  
+  require(parmigene)
+  library(foreach)
+  library(doParallel)
+  # precondition controls
+  if (!is.matrix(mexp)) 
+    stop("mexp must be a matrix")
+  if ( length(intersect(tf,rownames(mexp))) != length(tf) ) 
+    stop("tf must be included in rownames(mexp)")
+  if ( length(intersect(target,rownames(mexp))) != length(target) ) 
+    stop("target must be included in rownames(mexp)")
+  if ( length(intersect(rownames(kordering),rownames(mexp))) != nrow(kordering) ) 
+    stop("rownames(kordering) must be included in rownames(mexp)")
+  if ( length(intersect(colnames(kordering),colnames(mexp))) != ncol(mexp) ) 
+    stop("colnames(kordering) must be equal to colnames(mexp)")
+  
+  cl<-makeCluster(ncore)
+  registerDoParallel(cl)  
+
+  # ignoro il controllo di un range iniziale e finale con pochi sample
+  # mi aspetto che il k risieda piu internamente
+  inizio = round(ncol(kordering)*ignore)
+  range= inizio:(ncol(kordering)-inizio)
+  range = range[seq(1,length(range),S)] # prendo ogni S sample per ridurre il numero di k da controllare
+  
+  # struttura dati ritornata
+  # lista dei geni modulatori (cofattori o target). Per ogni modulatore una matrice 
+  tfmod = list()
+  
+  pb = txtProgressBar(min=1,max=nrow(kordering)*nboot*length(range),style=3) 
+  ri = 1 # contatore per la progress bar
+  for (x in rownames(kordering)) {
+    # per ogni modulatore candidato x-esimo
+    # matrice 4-dimensionale TF x target x range  x nboot
+    mi1k = array(0,dim=c(length(tf),length(target),length(range),nboot),dimnames=list(tf,target,range)) 
+    if (!positiveOnly) mikn = mi1k 
+    miall = mi1k
+    for (bi in 1:nboot) {
+      
+      for(k in range) {
+        ksample = sample(1:k,k*bfrac)
+        mi1k[,,as.character(k),bi] = knnmi.cross(mexp[tf,kordering[x,ksample]],mexp[target,kordering[x,ksample]])
+        mi1k[,,as.character(k),bi][mi1k[,,as.character(k),bi]<0] = 0
+
+        if(!positiveOnly) {
+          ksample = sample((ncol(kordering)-k):ncol(kordering),k*bfrac)
+          mikn[,,as.character(k),bi] = knnmi.cross(mexp[tf,kordering[x,ksample]],mexp[target,kordering[x,ksample]])
+          mikn[,,as.character(k),bi][mikn[,,as.character(k),bi]<0] = 0
+        }
+        
+        # null
+        ksample = sample(1:ncol(kordering),k*bfrac)
+        miall[,,as.character(k),bi] = knnmi.cross(mexp[tf,kordering[x,ksample]],mexp[target,kordering[x,ksample]])
+        miall[,,as.character(k),bi][miall[,,as.character(k),bi]<0] = 0
+        
+        setTxtProgressBar(pb, ri)
+        ri = ri + 1
+      }
+      
+
+    }
+    
+    # calcolo il rapporto tra le medie delle MI tra i due sample boot
+    # per ogni TF-target-k
+    midelta1k=log2(apply(mi1k,c(1,2,3),sum)/apply(miall,c(1,2,3),sum))
+    mipval1k = midelta1k*0+1
+    if (!positiveOnly) {
+      mideltakn=log2(apply(mikn,c(1,2,3),sum)/apply(miall,c(1,2,3),sum))
+      mipvalkn = mideltakn*0+1
+    }
+    
+    # effettuo il test tra i due sample boot
+    # per ogni TF-target-k sia direct che inverse
+    for(i in tf) {
+      mipval1k[i,,] = foreach(t=target, .combine='rbind') %:% 
+        foreach(j=as.character(range), .combine='c') %dopar% {
+          wilcox.test(mi1k[i,t,j,],miall[i,t,j,],alternative="greater")$p.value
+        }
+      if (!positiveOnly) {
+        mipvalkn[i,,] = foreach(t=target, .combine='rbind') %:% 
+          foreach(j=as.character(range), .combine='c') %dopar% {
+            wilcox.test(mikn[i,t,j,],miall[i,t,j,],alternative="greater")$p.value
+          }
+      }
+    }
+
+    # correzione dei pvalue
+    mipval1k[1:length(mipval1k)] = p.adjust(mipval1k[1:length(mipval1k)],method="fdr")
+    if (!positiveOnly) mipvalkn[1:length(mipvalkn)] = p.adjust(mipvalkn[1:length(mipvalkn)],method="fdr")
+    
+    
+    # per ogni modulatore candidato x-esimo 
+    # selezione del miglior k per ogni coppia TF->target
+    # memorizzo solo le coppie TF-target con pval sig
+    tmp = foreach(i=tf) %:% 
+      foreach(j=target, .combine='rbind') %do% {
+        b1k = which.min(mipval1k[i,j,])
+        pv1k = mipval1k[i,j,b1k]
+        retval=c()
+        if (!positiveOnly) {
+          bkn = which.min(mipvalkn[i,j,])
+          pvkn = mipvalkn[i,j,bkn]
+          if (pvkn <= sig) {
+            retval = rbind(c(FDR=pvkn,DELTA=mideltakn[i,j,bkn],K=as.numeric(names(bkn)),DIRECTION= -1))
+            rownames(retval)=j
+          }
+        }
+        if (pv1k <=sig) {
+          retval = rbind(retval,c(FDR=pv1k,DELTA=midelta1k[i,j,b1k],K=as.numeric(names(b1k)),DIRECTION= +1))
+          rownames(retval)=rep(j,nrow(retval))
+        }
+        retval
+      }
+    names(tmp) = tf
+    tfmod[[x]] = tmp
+  }
+  stopCluster(cl)
+  close(pb)
+  return(tfmod)
+}
+
